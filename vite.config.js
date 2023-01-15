9160b6d84e0622568b90a0e28a76e1889c921319
mazerty <mazerty@gmail.com>
2023-01-15 08:56:53 +0100

    simplified configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
+import tailwind from "tailwindcss"
+import defaultTheme from "tailwindcss/defaultTheme.js"
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [
+                tailwind({
+                    content: ["./index.html", "./src/**/*.jsx"],
+                    theme: {
+                        extend: {
+                            fontFamily: {
+                                sans: ["Ubuntu", ...defaultTheme.fontFamily.sans],
+                                specialelite: ["Special Elite", ...defaultTheme.fontFamily.sans]
+                            }
+                        }
+                    }
+                })
+            ]
+        }
+    }
 })
